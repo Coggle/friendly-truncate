fdd8d9e8d3f010de06c774fb49d2d5e2edc6385b
Iris Artin <iris@artins.org>
2023-10-18 22:53:23 -0400

    Fixed TS return types

--- index.d.ts
+++ index.d.ts
@@ -4,9 +4,9 @@ export function truncateEnd(
 	string: string,
 	length: number,
 	opts?: { ellipsis?: string; boundary?: RegExp; tolerance?: number },
-): any;
+): string;
 export function truncateMiddle(
 	string: string,
 	length: number,
 	opts?: { join?: string; boundary?: RegExp; tolerance?: number },
-): any;
+): string;
